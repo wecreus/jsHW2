f5b8518aa237032fa5011694df0434dd2e99d89a
wecreus <wecreus@gmail.com>
2019-09-16 01:13:31 +0300

    regex is done

--- script.js
+++ script.js
@@ -1,16 +1,35 @@
 let roles = ["Городничий", "Артемий Филиппович", "Аммос Федорович",
     "Лука Лукич", "Почтмейстер", "Бобчинский", "Добчинский",
     "Квартальный", "Частный пристав", "Анна Андреевна", "Марья Антоновна"];
-let textAreaValue;
 
 function compile(){
-    textAreaValue = document.querySelector(".textArea").value;
-    
+    //clearing arrays
+    for(let i = 0; i < roles.length; i++){
+        roles[i].linesIndex = [];
+        roles[i].lines = [];
+    }
+    let text = document.querySelector(".textArea").value;
+    if(text){
+        //finding every line of every role
+        for(let i = 0; i < roles.length; i++){
+            let myRegex = new RegExp(roles[i].name + "\..+", "g");
+            let match;
+            while ((match = myRegex.exec(text)) != null) {
+                roles[i].linesIndex.push(match.index);
+                roles[i].lines.push(match[0].slice(roles[i].name.length + 2));
+            }
+        }
+
+
+
+        console.log(roles);
+    }
+
+
 }
 
 // creating objects based on array roles
 for(let i = 0; i < roles.length; i++){
-    roles[i] = {name: roles[i], lines: []};
+    roles[i] = {name: roles[i], linesIndex: [], lines: []};
 }
-
 
