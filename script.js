f9da94006e1d94961ed447f8f7d0d4e121930f53
wecreus <wecreus@gmail.com>
2019-09-16 01:54:20 +0300

    l

--- script.js
+++ script.js
@@ -1,27 +1,32 @@
-let roles = ["Городничий", "Артемий Филиппович", "Аммос Федорович",
+let roles = ["Городничий", "Аммос Федорович",
     "Лука Лукич", "Почтмейстер", "Бобчинский", "Добчинский",
-    "Квартальный", "Частный пристав", "Анна Андреевна", "Марья Антоновна"];
+    "Квартальный", "Частный пристав", "Анна Андреевна", "Артемий Филиппович", "Марья Антоновна"];
 
 function compile(){
     //clearing arrays
+    let text = document.querySelector(".textArea").value;
     for(let i = 0; i < roles.length; i++){
         roles[i].linesIndex = [];
         roles[i].lines = [];
     }
-    let text = document.querySelector(".textArea").value;
     if(text){
+        let order = [];
         //finding every line of every role
         for(let i = 0; i < roles.length; i++){
             let myRegex = new RegExp(roles[i].name + "\..+", "g");
             let match;
+            let firstIteration = true;
             while ((match = myRegex.exec(text)) != null) {
+                if(firstIteration === true) {
+                    order.push(i);
+                    firstIteration = false;
+                }
                 roles[i].linesIndex.push(match.index);
                 roles[i].lines.push(match[0].slice(roles[i].name.length + 2));
             }
         }
 
-
-
+        console.log(order);
         console.log(roles);
     }
 
